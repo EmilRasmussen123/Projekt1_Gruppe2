1e299670b45298dfeca6f7cc81e71ceeeab3f864
AlushPJ <aabouhayt@gmail.com>
2024-10-21 12:58:31 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
+public class Salon {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Velkommen til dit nye Booking System");
+
     }
+}
+
+class kalender{
+
+}
+
+class booking{
+    String name;
+    String time;
+    String avaible;
+
+    booking(String name, String time, String avaible){
+        this.name = name;
+        this.time = time;
+        this.avaible = avaible;
+    }
+
+}
+
+class dato{
+
+
 }
