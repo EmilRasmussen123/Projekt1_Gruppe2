ae5b63069075790ce37073345bc6db99eed5d007
doukn <douknikadim10@gmail.com>
2024-10-28 11:46:39 +0100

    lavet om på koden fra harryhairy -> hairyharry

--- src/password.java
+++ src/password.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class password {
-    private static String PASSWORD = "harryhairy";
+    private static String PASSWORD = "hairyharry";
 
     // Metode der tjekker adgangskoden
     public static boolean verifyPassword() {
