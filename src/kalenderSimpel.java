7742cc623b5603210902905d168cf1042abc4617
hdtrap <peterpetertange@gmail.com>
2024-10-31 08:34:27 +0100

    Ryddet op i kalenderSimpel Ikke længere static - skal initieres i menu og bookingsystem

--- src/kalenderSimpel.java
+++ src/kalenderSimpel.java
@@ -4,32 +4,10 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class kalenderSimpel {
-    static FileWriter kalenderWriter;
-    static {
-        try {
-            kalenderWriter = new FileWriter("src//Kalender.txt", true);       //Writer - true -> tilføj til eksisterende dokument, null -> erstat gamle dokument
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static PrintWriter kalenderPrinter = new PrintWriter(kalenderWriter);     //Writer til at behandle dokument som konsol
 
-    static FileReader kalenderReader;                                         // Læser filen
-    static {
-        try {
-            kalenderReader = new FileReader("src//Kalender.txt");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static BufferedReader kalenderReaderBuffed = new BufferedReader(kalenderReader);       //Lader FileReader læse linjer i stedet for enkelte tegn
-
-    public kalenderSimpel() throws IOException {                                        //Konstruktør
-    }
-
-    //MIDLERTIDIGLISTE TØMMER AF EN ELLER ANDEN GRUND KALENDEREN NÅR MAN BRUGER DEN
-    //VED BRUG AF MIDLERTIDIGLISTE SKAL DER ALTID SKRIVES ET NYT DOKUMENT
     static ArrayList<String> midlertidigListe () throws IOException {
+        FileReader kalenderReader = new FileReader("src//Kalender.txt");
+        BufferedReader kalenderReaderBuffed = new BufferedReader(kalenderReader);
         ArrayList<String> bookinger = new ArrayList<>();              //Lav txt filen til en ArrayListe
         String dato = kalenderReaderBuffed.readLine();
         while (dato != null) {
@@ -58,35 +36,12 @@ public class kalenderSimpel {
     }       //Skal tilføjes bookingsystem, linje 10 - ArrayList<booking> bookings = kalenderSimpel.bookingObjektListe;
 
     static void nyBooking() throws IOException {      //Skal have booking.toString som input
-        kalenderWriter = new FileWriter("src//Kalender.txt");
+        FileWriter kalenderWriter = new FileWriter("src//Kalender.txt", true);
+        PrintWriter kalenderPrinter = new PrintWriter(kalenderWriter);
+
         for (booking booking : bookingSystem.bookings){
             kalenderPrinter.println(booking);
         }
         kalenderPrinter.close();
     }
-
-    static void sletBooking(String booking) throws IOException {            //Man kan ikke redigere direkte i et dokument, så man skal lave en ny liste og slette den gamle
-        ArrayList<String> bookinger = midlertidigListe();
-        bookinger.removeIf(tjek -> tjek.equals(booking));                   //Fjerner nævnte booking
-
-        FileWriter kalenderWriter = new FileWriter("src//Kalender.txt");    //Ikke true, da den opretter det nye, rettede dokument
-        PrintWriter kalenderPrinter = new PrintWriter(kalenderWriter);
-        for (String string : bookinger) {
-            kalenderPrinter.println(string);
-        }
-        kalenderPrinter.close();
-    }
-
-    static String dato(String booking) throws IOException {
-        ArrayList<String> bookinger = new ArrayList<>();                    //Lav en midlertidig liste
-        String dato = kalenderReaderBuffed.readLine();
-        while (dato != null) {
-            if (booking.substring(5,16).equals(dato.substring(5,16))) {     //Hvis datoen matcher
-                bookinger.add(dato.substring(18));                          //Så tilføj booking til en liste
-            }
-            dato = kalenderReaderBuffed.readLine();
-        }
-        return booking.substring(0,16)+",\t" + String.join("     ",bookinger);
-    }
-
 }
